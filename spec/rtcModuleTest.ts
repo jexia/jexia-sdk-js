5597cc87fde0e4ead7096e7b8b374681485fbd51
MihaiComan87 <m.coman@rocketmail.com>
2017-09-20 19:18:18 +0200

    Unsubscribe from RTC events (#109)
    * Unsubscribe from event per method, per dataset.
    
    * Add method to RTCModule to close the websocket connection.
    
    * Handle websocket connection being closed when trying to send stuff down the pipe.
    
    * Add error handling for bad websocket creation callbacks, using websocket that isn't initialized.
    
    * Add a whitespace in the concatenated error message.
    
    * Unit tests for the new real-time related error management.
    
    * Add unit test for unsubscribe action.

--- spec/rtcModuleTest.ts
+++ spec/rtcModuleTest.ts
@@ -1,3 +1,4 @@
+// tslint:disable:max-line-length
 import { Dataset } from "../src/api/dataops/dataset";
 import { RTCModule } from "../src/api/realtime/rtcModule";
 import { QueryExecuterBuilder } from "../src/internal/queryExecuterBuilder";
@@ -6,10 +7,21 @@ import { IRequestAdapter, IRequestOptions } from "../src/internal/requestAdapter
 class WebSocketMock {
   public url: string;
   public onopen: Function;
+  public onclose: Function;
+  private sendCallback: Function;
 
-  constructor(url: string) {
+  constructor(url: string, sendCallback?: Function) {
     this.url = url;
+    this.sendCallback = sendCallback ? sendCallback : (message: String) => message;
     setTimeout(() => this.onopen(), 100);
+  }
+
+  public close() {
+    setTimeout(() => this.onclose(), 100);
+  }
+
+  public send(message: String) {
+    this.sendCallback(message);
   }
 }
 
@@ -65,6 +77,27 @@ describe("RTCModule class", () => {
     });
   });
 
+  describe("when unsubscribing from events for a dataset", () => {
+    it("should send the proper JSON message to Sharky", (done) => {
+      let rtcm: any = new RTCModule(() => { return; }, (url: string) => new WebSocketMock(url) );
+      let datasetName: string = "datasetName";
+      let dataSchemaName: string = "dataSchemaName";
+      let actionName: string = "select";
+      let qef: QueryExecuterBuilder = new QueryExecuterBuilder(testurl, reqAdapterMock, tokenManagerMock);
+      let ds: Dataset = new Dataset(dataSchemaName, datasetName, qef);
+      rtcm.init(testurl, tokenManagerMock, reqAdapterMock).then( () => {
+        spyOn(rtcm, "send");
+        rtcm.unsubscribe(actionName, ds);
+        expect(rtcm.send).toHaveBeenCalledWith({ nsp: `rest.${actionName}.${dataSchemaName}.${datasetName}`,
+          type: "unsubscribe",
+        });
+        done();
+      }).catch( (error: Error) => {
+        done.fail("Initializing the RTCModule should not have failed.");
+      });
+    });
+  });
+
   describe("when receiving a message from Sharky", () => {
     it("should forward the message to the client defined callback", (done) => {
       let testCallback: Function = jasmine.createSpy("callback");
@@ -77,6 +110,64 @@ describe("RTCModule class", () => {
       }).catch( (error: Error) => {
         done.fail("Initializing the RTCModule should not have failed.");
       });
+    });
+  });
+
+  describe("when sending a message before opening the connection", () => {
+    const errMess: string = "The RTC Module seems to be missing a valid websocket object. Did you properly initialize the RTC Module by calling the init method?";
+
+    it("should throw an useful error", () => {
+      let rtcm: any = new RTCModule(() => { return; },
+        (url: string) => new WebSocketMock(url));
+      // you can find the actual error message in rtcmodule.ts / RTCModule file
+      // The idea is to make sure we catch the underlying exception and give
+      // a useful error message instead.
+      expect( () => rtcm.send("test")).toThrow(new Error(errMess));
+    });
+
+    it("should throw an useful error part 2", (done) => {
+      let rtcm: any = new RTCModule(() => { return; },
+        (url: string) => new WebSocketMock(url, () => { throw new Error("not opened"); }));
+      rtcm.init(testurl, tokenManagerMock, reqAdapterMock)
+        .then( () => { return; } )
+        .catch( (error: Error) => {
+          done.fail("Initializing the RTCModule should not have failed.");
+        });
+      expect( () => rtcm.send("test")).toThrow(new Error(errMess));
+      done();
+    });
+  });
+
+  describe("when sending a message after closing the connection", () => {
+    it("should throw a useful error", (done) => {
+      let rtcm: any = new RTCModule(() => { return; },
+        (url: string) => new WebSocketMock(url, () => { throw new Error("not opened"); }));
+      const errMess: string = "The connection seems to be closed. Did you properly initialize the RTC Module by calling the init method? Or maybe you terminated the RTC Module early?";
+      rtcm.init(testurl, tokenManagerMock, reqAdapterMock)
+        .then( () => {
+          rtcm.terminate().then( () => {
+            expect( () => rtcm.send("test")).toThrow(new Error(errMess));
+            done();
+          });
+        }).catch( (error: Error) => {
+          done.fail("Initializing the RTCModule should not have failed.");
+        });
+    });
+  });
+
+  describe("when supplying an invalid websocket creation callback", () => {
+    it("should signal the problem with an appropriate error", (done) => {
+      const errMess = "The callback you supplied for websocket creation threw an error. You might want to call it yourself and debug it to see what's wrong. Original error:";
+      const mockErr = "Something went terribly wrong.";
+      let rtcm: any = new RTCModule(() => { return; },
+        (url: string) => { throw new Error(mockErr); });
+      rtcm.init(testurl, tokenManagerMock, reqAdapterMock)
+        .then( () => {
+          done.fail("Initializing the RTCModule should have failed.");
+        }).catch( (error: Error) => {
+          expect(error.message).toEqual(`${errMess} ${mockErr}`);
+          done();
+        });
     });
   });
 });
--- src/api/realtime/rtcModule.ts
+++ src/api/realtime/rtcModule.ts
@@ -3,6 +3,15 @@ import { IModule } from "../core/module";
 import { TokenManager } from "../core/tokenManager";
 import { Dataset } from "../dataops/dataset";
 
+const NOT_OPEN_ERROR = "not opened";
+// tslint:disable-next-line:max-line-length
+const NOT_OPEN_MESSAGE = "The connection seems to be closed. Did you properly initialize the RTC Module by calling the init method? Or maybe you terminated the RTC Module early?";
+const NO_WESBSOCKET_PRESENT = "The RTC Module seems to be missing a valid websocket object. Did you properly initialize the RTC Module by calling the init method?";
+// tslint:disable-next-line:max-line-length
+const BAD_WEBSOCKET_CREATION_CALLBACK = "The websocket creation function you supplied did not return a valid websocket object.";
+// tslint:disable-next-line:max-line-length
+const ERROR_CREATING_WEBSOCKET = "The callback you supplied for websocket creation threw an error. You might want to call it yourself and debug it to see what's wrong.";
+
 export class RTCModule implements IModule {
   private websocket: WebSocket;
   private appUrl: string;
@@ -21,7 +30,16 @@ export class RTCModule implements IModule {
   public init(appUrl: string, tokenManager: TokenManager, requestAdapter: IRequestAdapter): Promise<RTCModule> {
     this.appUrl = appUrl;
     return tokenManager.token.then( (token) => {
-      this.websocket = this.websocketCreateCallback(this.buildSocketOpenUri(appUrl, token));
+      try {
+        this.websocket = this.websocketCreateCallback(this.buildSocketOpenUri(appUrl, token));
+      } catch (error) {
+        throw new Error(`${ERROR_CREATING_WEBSOCKET} Original error: ${error.message}`);
+      }
+
+      if (!this.websocket) {
+        return Promise.reject(BAD_WEBSOCKET_CREATION_CALLBACK);
+      }
+
       this.websocket.onmessage = (message: MessageEvent) => {
         this.messageReceivedCallback(message.data);
       };
@@ -40,8 +58,32 @@ export class RTCModule implements IModule {
     this.send({type: "subscribe", nsp: this.buildSubscriptionUri(method, dataset.schema, dataset.name)});
   }
 
+  public unsubscribe(method: string, dataset: Dataset) {
+    this.send({type: "unsubscribe", nsp: this.buildSubscriptionUri(method, dataset.schema, dataset.name)});
+  }
+
+  public terminate() {
+    return new Promise( (resolve, reject) => {
+      this.websocket.onclose = () => resolve();
+      this.websocket.onerror = (err) => {
+        reject(err);
+      };
+      this.websocket.close();
+    });
+  }
+
   private send(message: object) {
-    this.websocket.send(JSON.stringify(message));
+    if (!this.websocket) {
+      throw new Error(NO_WESBSOCKET_PRESENT);
+    }
+    try {
+      this.websocket.send(JSON.stringify(message));
+    } catch (error) {
+      if  (error.message === NOT_OPEN_ERROR) {
+        throw new Error(NOT_OPEN_MESSAGE);
+      }
+      throw new Error(error);
+    }
   }
 
   private buildSubscriptionUri(method: string, schema: string, datasetName: string) {
