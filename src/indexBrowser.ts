7de3d17654804f7c99dc04cf3e6fa230e7d324ab
Mihai Coman <m.coman@rocketmail.com>
2017-10-23 11:49:15 +0200

    Index files fixes (#143)
    * Add jexiaClient() factory method to browser index.
    
    * Add jexiaClient() factory method to the node index.
    
    * Bind fetch to window to make sure internal SDK fetch calls are working.
    
    * Expose field() filtering functionality through the index files..
    
    * Add realTimeModule() factory function to create RTC module objects using appropriate defaults for browser.
    
    * Add dataOperations() factory method to provide an instance of the DataOperationsModule.

--- src/indexBrowser.ts
+++ src/indexBrowser.ts
@@ -1,11 +1,28 @@
 import Client from "./api/core/client";
 import { IModule } from "./api/core/module";
 import DataOperationsModule from "./api/dataops/dataOperationsModule";
+import { combineCriteria, field } from "./api/dataops/filteringApi";
 import { RTCModule } from "./api/realtime/rtcModule";
 
+function jexiaClient(): Client {
+  return new Client(window.fetch.bind(window));
+}
+
+function realTimeModule(messageReceivedCallback: Function): RTCModule {
+  return new RTCModule(messageReceivedCallback, (appUrl: string) => {
+    return new WebSocket(appUrl);
+  });
+}
+
+function dataOperations(): DataOperationsModule {
+  return new DataOperationsModule();
+}
+
 export {
-  DataOperationsModule,
-  Client,
+  combineCriteria,
+  dataOperations,
+  field,
+  jexiaClient,
   IModule,
-  RTCModule,
+  realTimeModule,
 };
--- src/indexNode.ts
+++ src/indexNode.ts
@@ -1,11 +1,26 @@
 import Client from "./api/core/client";
 import { IModule } from "./api/core/module";
 import DataOperationsModule from "./api/dataops/dataOperationsModule";
+import { combineCriteria, field } from "./api/dataops/filteringApi";
 import { RTCModule } from "./api/realtime/rtcModule";
 
+function jexiaClient(fetchFunc: Function): Client {
+  return new Client(fetchFunc);
+}
+
+function realTimeModule(messageReceivedCallback: Function, webSocketCreateCallback: Function): RTCModule {
+  return new RTCModule(messageReceivedCallback, webSocketCreateCallback);
+}
+
+function dataOperations(): DataOperationsModule {
+  return new DataOperationsModule();
+}
+
 export {
-  DataOperationsModule,
-  Client,
+  combineCriteria,
+  dataOperations,
+  field,
+  jexiaClient,
   IModule,
-  RTCModule,
+  realTimeModule,
 };
