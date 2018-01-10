a54a1fc65be00e4cda2a36d1535ee97e59b1f1a1
William Grasel <d.rufos@gmail.com>
2018-01-10 16:55:42 -0200

    refactor(auth): base abstract class for simple auth methods
    removing code duplication on simple auth merhods

--- src/api/auth/apiKeyAuth.ts
+++ src/api/auth/apiKeyAuth.ts
@@ -1,49 +1,16 @@
 import { API } from "../../config/config";
-import { IRequestAdapter, Methods } from "../../internal/requestAdapter";
-import { IAuthAdapter, IAuthOptions, IAuthToken, Tokens } from "../core/tokenManager";
+import { IAuthOptions } from "../core/tokenManager";
+import { BaseAuth } from "./BaseAuth";
 
-export class ApiKeyAuth implements IAuthAdapter {
+export class ApiKeyAuth extends BaseAuth {
 
-  public login(opts: IAuthOptions, requestAdapter: IRequestAdapter): Promise<IAuthToken> {
-    return requestAdapter
-      .execute(buildLoginUrl(opts.projectID), {
-        body: {
-          key: opts.key,
-          secret: opts.secret,
-        },
-        method: Methods.POST,
-      })
-      .then((newTokens: Tokens) => {
-        return ({ token: newTokens.token, refreshToken: newTokens.refresh_token } as IAuthToken);
-      })
-      /* catch login error */
-      .catch((err: Error) => {
-        /* add specific information to error */
-        throw new Error(`Unable to authenticate: ${err.message}`);
-      });
+  protected authUrl = API.AUTH.API_KEY;
+
+  protected getLoginRequestBody(opts: IAuthOptions) {
+    return {
+      key: opts.key,
+      secret: opts.secret,
+    };
   }
 
-  public refresh(
-    tokenPair: Promise<IAuthToken>,
-    requestAdapter: IRequestAdapter,
-    projectID: string,
-  ): Promise<IAuthToken> {
-    /* wait for tokens */
-    return tokenPair.then((tokens: IAuthToken) => requestAdapter.execute(buildLoginUrl(projectID), {
-      body: { refresh_token: tokens.refreshToken },
-      headers: { Authorization: tokens.token },
-      method: Methods.PATCH,
-    }))
-      /* convert response to IAuthToken interface */
-      .then((newTokens: Tokens) => ({ token: newTokens.token, refreshToken: newTokens.refresh_token } as IAuthToken))
-      /* catch refresh token error */
-      .catch((err: Error) => {
-        /* add specific information to error */
-        throw new Error(`Unable to refresh token: ${err.message}`);
-      });
-  }
-}
-
-function buildLoginUrl(projectID: string): string {
-  return `${API.PROTOCOL}://${projectID}.${API.HOST}.${API.DOMAIN}:${API.PORT}/${API.AUTH.API_KEY}`;
 }
--- src/api/auth/userCredentialsAuth.ts
+++ src/api/auth/userCredentialsAuth.ts
@@ -1,49 +1,16 @@
 import { API } from "../../config/config";
-import { IRequestAdapter, Methods } from "../../internal/requestAdapter";
-import { IAuthAdapter, IAuthOptions, IAuthToken, Tokens } from "../core/tokenManager";
+import { IAuthOptions } from "../core/tokenManager";
+import { BaseAuth } from "./BaseAuth";
 
-export class UserCredentialsAuth implements IAuthAdapter {
+export class UserCredentialsAuth extends BaseAuth {
 
-  public login(opts: IAuthOptions, requestAdapter: IRequestAdapter): Promise<IAuthToken> {
-    return requestAdapter
-      .execute(buildLoginUrl(opts.projectID), {
-        body: {
-          email: opts.key,
-          password: opts.secret,
-        },
-        method: Methods.POST,
-      })
-      .then((newTokens: Tokens) => {
-        return ({ token: newTokens.token, refreshToken: newTokens.refresh_token } as IAuthToken);
-      })
-      /* catch login error */
-      .catch((err: Error) => {
-        /* add specific information to error */
-        throw new Error(`Unable to authenticate: ${err.message}`);
-      });
+  protected authUrl = API.AUTH.USER_CREDENTIALS;
+
+  protected getLoginRequestBody(opts: IAuthOptions) {
+    return {
+      email: opts.key,
+      password: opts.secret,
+    };
   }
 
-  public refresh(
-    tokenPair: Promise<IAuthToken>,
-    requestAdapter: IRequestAdapter,
-    projectID: string,
-  ): Promise<IAuthToken> {
-    /* wait for tokens */
-    return tokenPair.then((tokens: IAuthToken) => requestAdapter.execute(buildLoginUrl(projectID), {
-      body: { refresh_token: tokens.refreshToken },
-      headers: { Authorization: tokens.token },
-      method: Methods.PATCH,
-    }))
-      /* convert response to IAuthToken interface */
-      .then((newTokens: Tokens) => ({ token: newTokens.token, refreshToken: newTokens.refresh_token } as IAuthToken))
-      /* catch refresh token error */
-      .catch((err: Error) => {
-        /* add specific information to error */
-        throw new Error(`Unable to refresh token: ${err.message}`);
-      });
-  }
-}
-
-function buildLoginUrl(projectID: string): string {
-  return `${API.PROTOCOL}://${projectID}.${API.HOST}.${API.DOMAIN}:${API.PORT}/${API.AUTH.USER_CREDENTIALS}`;
 }
