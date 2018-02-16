adedd1c17332817df0b21b55e4405470d859e07b
William Grasel <d.rufos@gmail.com>
2018-02-16 17:48:10 +0100

    refactor(test-utils): new test utils functions to be used

--- spec/testUtils.ts
+++ spec/testUtils.ts
@@ -1,3 +1,4 @@
+  // tslint:disable:no-string-literal
 import { TokenManager } from "../src/api/core/tokenManager";
 import { RequestExecuter } from "../src/internal/executer";
 import { QueryExecuterBuilder } from "../src/internal/queryExecuterBuilder";
@@ -56,4 +57,95 @@ export function createRequestExecuterMock(projectID: string, datasetName: string
   let tokenManagerMock = new TokenManager(reqAdapterMock);
   let qefMock = new QueryExecuterBuilder(projectID, reqAdapterMock, tokenManagerMock);
   return qefMock.createQueryExecuter(datasetName);
+}
+
+export interface ISpyOptions {
+  returnValue?: any;
+  callFake?: (...args: Function[]) => any;
+  callThrough?: boolean;
+}
+
+interface IConstructor<T> {
+  new(...args: any[]): T;
+}
+
+export function createMockFor<T>
+    (obj: IConstructor<T>, spyOptions?: ISpyOptions, defaultProps?: object): jasmine.SpyObj<T> {
+  const methodNames = Array.isArray(obj) ? obj : getMethodNamesOf(obj);
+  if (!methodNames.length && spyOptions) {
+    throw new Error("Given blueprint has no methods to spyOn");
+  }
+  const spyObj = methodNames.length ?
+    jasmine.createSpyObj((obj as any)["name"] || methodNames[0], methodNames) : {};
+
+  if (spyOptions) {
+    methodNames.forEach((m) => { setSpyOptions(spyObj[m], spyOptions); });
+  }
+  if (defaultProps) {
+    Object.entries(defaultProps)
+      .forEach(([key, value]: any) => spyObj[key] = value);
+  }
+  return spyObj;
+}
+
+export function mockPrototypeOf(obj: any, spyOptions: ISpyOptions = {}, defaultProps: any = {}): void {
+  getMethodNamesOf(obj)
+    .forEach((m) => {
+      const descriptor = Object.getOwnPropertyDescriptor(obj.prototype, m);
+      if (descriptor && descriptor.get) {
+        const getter = spyOnProperty(obj.prototype, m, "get");
+        const defaultValue = defaultProps[m];
+        if (defaultValue) {
+          getter.and.returnValue(defaultValue);
+        }
+      }
+      if (descriptor && descriptor.set) {
+        spyOnProperty(obj.prototype, m, "set");
+      }
+      if (!descriptor || !descriptor.set && !descriptor.get) {
+        setSpyOptions(spyOn(obj.prototype, m), spyOptions);
+      }
+    });
+}
+
+function getMethodNamesOf(obj: any) {
+  return Object.getOwnPropertyNames(obj.prototype).filter((i) => i !== "constructor");
+}
+
+function setSpyOptions(spy: jasmine.Spy, opts: ISpyOptions = {}) {
+  if (opts.returnValue !== undefined) {
+    spy.and.returnValue(opts.returnValue);
+  }
+  if (opts.callFake !== undefined) {
+    spy.and.callFake(opts.callFake);
+  }
+  if (opts.callThrough !== undefined) {
+    spy.and.callThrough();
+  }
+}
+
+export function deepFreeze<T>(obj: T): T {
+  Object.getOwnPropertyNames(obj)
+    .forEach((name) => {
+      const prop = (obj as any)[name];
+      if (typeof prop === "object" && prop !== null) {
+        deepFreeze(prop);
+      }
+    });
+  return Object.freeze(obj);
+}
+
+export function deepClone<T>(obj: T): T {
+  if (Array.isArray(obj)) {
+    return obj.map(deepClone) as any;
+  }
+  const clone: any = Object.assign({}, obj);
+  Object.getOwnPropertyNames(clone)
+    .forEach((name) => {
+      const prop = clone[name];
+      if (typeof prop === "object" && prop !== null) {
+        clone[name] = deepClone(prop);
+      }
+    });
+  return clone;
 }
