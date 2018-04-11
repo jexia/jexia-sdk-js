bbfc9b4e5f1c88583ffde3ac348a5a079d6c1d29
William Grasel <d.rufos@gmail.com>
2018-04-11 15:11:49 -0300

    test(datatset): refactoring and coverage increasing

--- spec/datasetTest.ts
+++ spec/datasetTest.ts
@@ -1,21 +1,31 @@
 import { Dataset } from "../src/api/dataops/dataset";
+import { DeleteQuery } from "../src/api/dataops/deleteQuery";
+import { InsertQuery } from "../src/api/dataops/insertQuery";
+import { SelectQuery } from "../src/api/dataops/selectQuery";
+import { UpdateQuery } from "../src/api/dataops/updateQuery";
 import { RequestExecuter } from "../src/internal/executer";
 import { createMockFor } from "./testUtils";
 
 describe("Dataset class", () => {
 
-  describe("when instantiating a dataset object directly", () => {
-    it("should create a valid object", () => {
-      const dataset = new Dataset("test", createMockFor(RequestExecuter));
-      expect(dataset).toBeDefined();
-    });
+  it("should be able start a select query", () => {
+    const dataset = new Dataset("test", createMockFor(RequestExecuter));
+    expect(dataset.select() instanceof SelectQuery).toBeTruthy();
   });
 
-  describe("when instantiating a dataset object directly", () => {
-    it("should be able to call required methods on dataset", () => {
-      const dataset = new Dataset("test", createMockFor(RequestExecuter));
-      expect(dataset.select()).toBeDefined();
-    });
+  it("should be able start a update query", () => {
+    const dataset = new Dataset("test", createMockFor(RequestExecuter));
+    expect(dataset.update({}) instanceof UpdateQuery).toBeTruthy();
+  });
+
+  it("should be able start a insert query", () => {
+    const dataset = new Dataset("test", createMockFor(RequestExecuter));
+    expect(dataset.insert([{}]) instanceof InsertQuery).toBeTruthy();
+  });
+
+  it("should be able start a delete query", () => {
+    const dataset = new Dataset("test", createMockFor(RequestExecuter));
+    expect(dataset.delete() instanceof DeleteQuery).toBeTruthy();
   });
 
 });
