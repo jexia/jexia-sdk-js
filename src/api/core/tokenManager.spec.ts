694ea435d5582c2954fc6c6eab6d0d57c6e6a22f
William Grasel <d.rufos@gmail.com>
2018-05-21 18:19:30 -0300

    fix(TokenManager): fix refresh token time inconsistency when initializing

--- src/api/core/tokenManager.spec.ts
+++ src/api/core/tokenManager.spec.ts
@@ -38,23 +38,21 @@ describe("Class: TokenManager", () => {
       tm = new TokenManager(requestAdapterMockFactory().succesfulExecution({token: "validToken", refresh_token: "validRefreshToken"}));
     });
 
-    it("should throw an error if application URL is not provided", (done) => {
-      (new TokenManager(requestAdapterMockFactory().genericSuccesfulExecution()))
+    it("should throw an error if application URL is not provided", async () => {
+      await (new TokenManager(requestAdapterMockFactory().genericSuccesfulExecution()))
         .init({projectID: "", key: "validKey", secret: "validSecret"})
-        .then(() => done.fail("should throw app URL error"))
+        .then(() => { throw new Error("should have throw app URL error"); })
         .catch((err: Error) => {
           expect(err).toEqual(new Error("Please supply a valid Jexia project ID."));
-          done();
         });
     });
 
-    it("should throw an error if key or secret is not provided", (done) => {
-      (new TokenManager(requestAdapterMockFactory().genericSuccesfulExecution()))
+    it("should throw an error if key or secret is not provided", async () => {
+      await (new TokenManager(requestAdapterMockFactory().genericSuccesfulExecution()))
         .init({projectID: validProjectID, key: "", secret: ""})
-        .then(() => done.fail("should throw credentials error"))
+        .then(() => { throw new Error("should have throw authentication error"); })
         .catch((err: Error) => {
           expect(err).toEqual(new Error("Please provide valid application credentials."));
-          done();
         });
     });
 
@@ -68,18 +66,57 @@ describe("Class: TokenManager", () => {
         });
     });
 
-    it("should have valid token and refresh token if authorization succeeded", (done) => {
-      tm.init({projectID: validProjectID, key: "validKey", secret: "validSecret"})
-        .then((output: TokenManager) => {
-          expect(output instanceof TokenManager).toBe(true);
-          return output["tokens"];
-        })
-        .then((tokens: IAuthToken) => {
-          expect(tokens.token).toBe("validToken");
-          expect(tokens.refreshToken).toBe("validRefreshToken");
-          done();
-        })
-        .catch((err: Error) => done.fail(`init should not have failed: ${err.message}`));
+    it("should try login when storage has no token", async () => {
+      spyOn(TokenStorage.getStorageAPI(), "isEmpty").and.returnValue(true);
+      spyOn(tm as any, "login").and.returnValue(Promise.resolve());
+      const opts = { ...validOpts(), authMethod: () => authMethodMock().authMethod };
+      await tm.init(opts);
+      expect(tm["login"]).toHaveBeenLastCalledWith(opts);
+    });
+
+    it("should fail initialization when login fails", async () => {
+      spyOn(TokenStorage.getStorageAPI(), "isEmpty").and.returnValue(true);
+      const loginError = "loginError";
+      spyOn(tm as any, "login").and.returnValue(Promise.reject(loginError));
+      const opts = { ...validOpts(), authMethod: () => authMethodMock().authMethod };
+      try {
+        await tm.init(opts);
+        throw new Error("init should have failed!");
+      } catch (error) {
+        expect(error).toBe(loginError);
+      }
+    });
+
+    it("should try refresh session when storage has no token", async () => {
+      spyOn(TokenStorage.getStorageAPI(), "isEmpty").and.returnValue(false);
+      spyOn(tm as any, "refresh").and.returnValue(Promise.resolve());
+      const opts = { ...validOpts(), authMethod: () => authMethodMock().authMethod };
+      await tm.init(opts);
+      expect(tm["refresh"]).toHaveBeenLastCalledWith(opts.projectID);
+    });
+
+    it("should fail initialization when refresh session fails", async () => {
+      spyOn(TokenStorage.getStorageAPI(), "isEmpty").and.returnValue(false);
+      const loginError = "loginError";
+      spyOn(tm as any, "refresh").and.returnValue(Promise.reject(loginError));
+      const opts = { ...validOpts(), authMethod: () => authMethodMock().authMethod };
+      try {
+        await tm.init(opts);
+        throw new Error("init should have failed!");
+      } catch (error) {
+        expect(error).toBe(loginError);
+      }
+    });
+
+    it("should result itself at then promise when authorization succeeded", async () => {
+      const result = await tm.init(validOpts());
+      expect(result).toBe(tm);
+    });
+
+    it("should have valid token and refresh token if authorization succeeded", async () => {
+      const tokens = await tm.init(validOpts()).then((out) => out["tokens"]);
+      expect(tokens.token).toBe("validToken");
+      expect(tokens.refreshToken).toBe("validRefreshToken");
     });
 
     it("should throw an error if the token is accessed before login", (done) => {
@@ -161,19 +198,31 @@ describe("Class: TokenManager", () => {
 
   describe("when refreshing the token", () => {
     it("should use the auth method adapter correct", (done) => {
-      const { authMethod, resultValue } = authMethodMock();
+      const { authMethod } = authMethodMock();
       const opts = { ...validOpts(), authMethod: () => authMethod };
       const tm = tokenManagerWithTokens();
-      spyOn(tm["storage"], "setTokens");
       tm.init(opts)
-        .then((tokenManager) => {
-          const tokens = tokenManager["tokens"];
+        .then(({ tokens, requestAdapter }) => {
           setTimeout(() => {
             expect(authMethod.refresh).toHaveBeenCalledWith(
               tokens,
-              tokenManager["requestAdapter"],
+              requestAdapter,
               opts.projectID,
             );
+            done();
+          }, opts.refreshInterval + 10);
+        })
+        .catch((err: Error) => done.fail(`init should not have failed: ${err.message}`));
+    });
+
+    it("should set the new tokens after a success refresh", (done) => {
+      const { authMethod, resultValue } = authMethodMock();
+      const opts = { ...validOpts(), authMethod: () => authMethod };
+      const tm = tokenManagerWithTokens();
+      tm.init(opts)
+        .then(({ tokens, requestAdapter }) => {
+          spyOn(tm["storage"], "setTokens");
+          setTimeout(() => {
             expect(tm["storage"].setTokens).toHaveBeenCalledWith(resultValue);
             done();
           }, opts.refreshInterval + 10);
@@ -183,7 +232,7 @@ describe("Class: TokenManager", () => {
 
     it("should throw an error on refresh token failure", (done) => {
       tokenManagerWithTokens()
-        .init({projectID: validProjectID, key: "validKey", secret: "validSecret"})
+        .init(validOpts())
         .then((tokenManager: TokenManager) => {
           tokenManager["requestAdapter"] = requestAdapterMockFactory().failedExecution("refresh error.");
           tokenManager["refresh"](validProjectID)
@@ -197,18 +246,17 @@ describe("Class: TokenManager", () => {
     });
 
     it("should have updated token after successful auto refresh", (done) => {
+      const { authMethod, resultValue } = authMethodMock();
+      const opts = validOpts();
       tokenManagerWithTokens()
-        .init(validOpts())
+        .init({ ...opts, authMethod: () => authMethod })
         .then((tokenManager: TokenManager) => {
+          spyOn(tokenManager["storage"], "setTokens");
           tokenManager["requestAdapter"] = requestAdapterMockFactory().succesfulExecution({token: "updatedToken", refresh_token: "updatedRefreshToken"});
           setTimeout(() => {
-            tokenManager.token
-              .then((token: string) => {
-                expect(token).toBe("updatedToken");
-                done();
-              })
-              .catch((err: Error) => done.fail(`refresh should not have failed: ${err.message}`));
-          }, 700);
+            expect(tokenManager["storage"].setTokens).toHaveBeenLastCalledWith(resultValue);
+            done();
+          }, opts.refreshInterval + 10);
         })
         .catch((err: Error) => done.fail(`init should not have failed: ${err.message}`));
     });
