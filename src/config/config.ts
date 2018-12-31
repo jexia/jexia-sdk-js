12d657c9e521c926406a7f2e901cfc1c2e9e54b5
Kutsenko Andrey <andr.donetsk@gmail.com>
2018-12-31 12:56:28 +0200

    fix(query): remove record api

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,12 +5,7 @@ export const API = {
     API_KEY: "auth",
   },
   DATA: {
-    ENDPOINT: {
-      /* We are going to use REST API endpoint for create (insert) records and delete records by id(s) */
-      REST: "ds/r",
-      /* Use QUERY endpoint for any queries that have conditions, such as select, update and delete */
-      QUERY: "ds/q",
-    },
+    ENDPOINT: "ds",
   },
   FILES: {
     ENDPOINT: "fs",
