2491b748d3c76f21a886f380a958f7ae14b815fd
Kutsenko Andrey <andr.donetsk@gmail.com>
2019-05-16 09:20:56 +0300

    review fixes

--- src/api/dataops/dataset.spec.ts
+++ src/api/dataops/dataset.spec.ts
@@ -1,6 +1,7 @@
 // tslint:disable:no-string-literal
 import { createMockFor } from "../../../spec/testUtils";
 import { RequestExecuter } from "../../internal/executer";
+import { ResourceType } from "../core/resource";
 import { Dataset } from "./dataset";
 import { DeleteQuery } from "./queries/deleteQuery";
 import { InsertQuery } from "./queries/insertQuery";
@@ -9,6 +10,11 @@ import { UpdateQuery } from "./queries/updateQuery";
 
 describe("Dataset class", () => {
   describe("after initiating", () => {
+    it("should have correct resource type", () => {
+      const dataset = new Dataset("test", createMockFor(RequestExecuter));
+      expect(dataset.resourceType).toEqual(ResourceType.Dataset);
+    });
+
     it("dataset name should be set", () => {
       const dataset = new Dataset("test", createMockFor(RequestExecuter));
       expect(dataset.name).toEqual("test");
