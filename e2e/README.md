3dee3c4b7004f6fe9ace2f22691efea23ac4b36f
Kutsenko Andrey <andr.donetsk@gmail.com>
2019-05-17 14:21:29 +0300

    task(fileset): append data field
    - add fileOperationsModule tests
    - add upload file browser example
    - fix run e2e tests readme

--- e2e/README.md
+++ e2e/README.md
@@ -4,9 +4,18 @@
 ```
 # linux, macos
 > NODE_TLS_REJECT_UNAUTHORIZED='0' JEXIA_DEV_DOMAIN='stag | com' E2E_PROJECT_ID='<project_id>' 
-  E2E_EMAIL='<user_email>' E2E_PASSWORD='user_password' npm run test:e2e
+  E2E_EMAIL='<user_email>' E2E_PASSWORD='user_password' 
+  RECAPTCHA_TOKEN='E2E.Tests.Recaptcha.Token' npm run test:e2e
 
 # windows
 > .\node_modules\.bin\cross-env NODE_TLS_REJECT_UNAUTHORIZED='0' JEXIA_DEV_DOMAIN='stag | com' E2E_PROJECT_ID='<project_id>' 
-  E2E_EMAIL='<user_email>' E2E_PASSWORD='<user_password>' npm run test:e2e
+  E2E_EMAIL='<user_email>' E2E_PASSWORD='<user_password>' 
+  RECAPTCHA_TOKEN='E2E.Tests.Recaptcha.Token' npm run test:e2e
+```
+
+## For running Fileset Module tests you need to provide additional env variables
+```
+AWS_KEY
+AWS_SECRET
+AWS_BUCKET
 ```
---
+++ src/api/fileops/fileOperationsModule.spec.ts
@@ -0,0 +1,73 @@
+// tslint:disable:no-string-literal
+import * as faker from 'faker';
+import * as FormData from 'form-data';
+import { createMockFor, deepClone, fetchWithRequestMockOk, validClientOpts } from '../../../spec/testUtils';
+import { jexiaClient } from '../../node';
+import { FileOperationsModule } from './fileOperationsModule';
+import { Fileset } from './fileset';
+
+fdescribe('File Operations Module', () => {
+
+  const createSubject = ({
+    formDataMock = createMockFor(FormData),
+    client = jexiaClient(fetchWithRequestMockOk),
+  } = {}) => {
+    const subject = new FileOperationsModule(formDataMock);
+    return {
+      subject,
+      formDataMock,
+      client,
+      moduleInit() {
+        return client.init(deepClone(validClientOpts), subject);
+      },
+    };
+  };
+
+  describe('when initializing', () => {
+    it('should resolve initialization automatically after configure the injector', async () => {
+      const { subject, moduleInit } = createSubject();
+      await moduleInit();
+      expect(subject['injector']).toBeDefined();
+    });
+  });
+
+  describe('when gets a fileset', () => {
+    it('should update auth options if auth alias is provided', async () => {
+      const { subject, moduleInit } = createSubject();
+      await moduleInit();
+      const fileset = subject.fileset('testFileset', 'testAlias');
+      const fileUploaderConfig = fileset['fileUploader']['config'];
+      expect(fileUploaderConfig.auth).toEqual('testAlias');
+    });
+
+    it('should not update auth options if auth alias is not provided)', async () => {
+      const { subject, moduleInit } = createSubject();
+      await moduleInit();
+      const fileset = subject.fileset('testFileset');
+      const fileUploaderConfig = fileset['fileUploader']['config'];
+      expect(fileUploaderConfig.auth).not.toBeDefined();
+    });
+
+    it('should get fileset with the same name', async () => {
+      const filesetName = faker.random.word();
+      const { subject, moduleInit } = createSubject();
+      await moduleInit();
+      expect(subject.fileset(filesetName).name).toEqual(filesetName);
+    });
+
+    it('should not have fileset in injector without providing a name', async () => {
+      const { subject, moduleInit } = createSubject();
+      await moduleInit();
+      expect(() => subject['injector'].get(Fileset)).toThrow('No provider for Fileset!');
+    });
+  });
+
+  describe("when terminating", () => {
+    it("should resolve automatically", async () => {
+      const { subject } = createSubject();
+      const result = await subject.terminate();
+      expect(result).toBe(subject);
+    });
+  });
+
+});
