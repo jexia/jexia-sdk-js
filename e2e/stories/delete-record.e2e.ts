c643151a4660af6cba70a7a6b96f50f4e65fd7fe
Marlos Borges <marlos.borges@gmail.com>
2019-05-27 17:32:36 +0200

    Add e2e tests for delete datasets (#72)
    * test(e2e): delete dataset
    
    * fix: review comments

---
+++ e2e/stories/delete-record.e2e.ts
@@ -0,0 +1,50 @@
+import * as faker from "faker";
+import * as Joi from "joi";
+import { field } from "../../src";
+import { cleaning, DEFAULT_DATASET, dom, init } from "../teardowns";
+import { MESSAGE } from './../../src/config/message';
+
+const joiAssert = Joi.assert;
+
+jest.setTimeout(15000); // for the unstable internet connection
+
+describe("delete record REST API", async () => {
+  const getDataSet = () => dom.dataset(DEFAULT_DATASET.NAME);
+  const BAD_REQUEST = new Error(`${MESSAGE.CORE.BACKEND_ERROR}400 Bad Request`);
+
+  beforeAll(async () => init());
+
+  afterAll(async () => cleaning());
+
+  it("deletes single record by id", async () => {
+    const [record] = await getDataSet()
+      .insert({ [DEFAULT_DATASET.FIELD]: faker.name.findName() })
+      .execute();
+
+    const isRecord = field("id").isEqualTo(record.id);
+
+    await getDataSet()
+      .delete()
+      .where(isRecord)
+      .execute();
+
+    const result = await getDataSet()
+      .select()
+      .where(isRecord)
+      .execute();
+
+    joiAssert(result, Joi.empty());
+  });
+
+  it("should throw error under invalid where condition", async () => {
+    try {
+      await getDataSet()
+        .delete()
+        .where(field("id").isEqualTo(faker.random.uuid()))
+        .execute();
+    } catch (e) {
+      joiAssert(e, BAD_REQUEST);
+    }
+  });
+
+});
