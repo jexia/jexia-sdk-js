82b3b540d96568375a063b9ebf675677fb482628
Kutsenko Andrey <andr.donetsk@gmail.com>
2019-09-24 10:29:10 +0300

    fix channel type

--- src/api/realtime/realTimeModule.ts
+++ src/api/realtime/realTimeModule.ts
@@ -7,7 +7,7 @@ import { Dataset } from "../dataops/dataset";
 import { IFormData } from "../fileops/fileops.interfaces";
 import { Fileset } from "../fileops/fileset";
 import { Channel } from "./channel";
-import { IWebSocket, IWebSocketBuilder, RealTimeEventMessage, WebSocketState } from "./realTime.interfaces";
+import { IWebSocket, IWebSocketBuilder, WebSocketState } from "./realTime.interfaces";
 import { watch } from "./watch";
 import * as websocket from "./websocket";
 
@@ -96,6 +96,7 @@ export class RealTimeModule implements IModule {
 
   /**
    * Return configuration
+   * @internal
    */
   public getConfig(): { [moduleName: string]: ModuleConfiguration } {
     return { rtc: {} };
@@ -103,10 +104,10 @@ export class RealTimeModule implements IModule {
 
   /**
    * Return a channel
-   * @param name
+   * @param {string} name Name of the channel
    */
-  public channel<T = any>(name: string): Channel<RealTimeEventMessage<T>> {
-    return new Channel(this.websocket, name);
+  public channel<T = any>(name: string): Channel<T> {
+    return new Channel<T>(this.websocket, name);
   }
 
   /**
