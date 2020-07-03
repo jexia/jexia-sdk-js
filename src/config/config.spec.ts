76d52f7a4eae5ac608ef48d56f1c352190aea75b
Marlos Borges <marlos.borges@gmail.com>
2020-07-03 18:30:32 +0200

    feat(rtc-url): extract from projectURL parameter

--- src/config/config.spec.ts
+++ src/config/config.spec.ts
@@ -1,48 +1,40 @@
 import * as faker from "faker";
 import { IAuthOptions } from "../api/core/tokenManager";
-import { API, API_SUFFIX, DEFAULT_PROJECT_ZONE, getApiUrl } from "./config";
+import {
+  API,
+  API_SUFFIX,
+  DEFAULT_PROJECT_ZONE,
+  getApiUrl,
+  stripUrlSlashes,
+  getRtcUrl,
+  REALTIME_SUFFIX,
+  getZone,
+} from "./config";
+
+const createAuthOptions = ({
+  projectID = faker.random.uuid(),
+  zone = faker.helpers.randomize(["nl00", "nl01", "nl03"]) as string | null,
+  projectURL = null as string | null,
+  config = { projectID, zone, projectURL } as IAuthOptions,
+} = {}) => config;
 
 describe("get api url", () => {
-  const createAuthOptions = ({
-    projectID = faker.random.uuid(),
-    zone = faker.helpers.randomize(["NL00", "NL01", "NL03"]) as string | null,
-    projectURL = null as string | null,
-    config = { projectID, zone, projectURL } as IAuthOptions,
-  } = {}) => {
-    return config;
-  };
-
   it("should return composed url", () => {
     const authOptions = createAuthOptions();
 
-    expect(getApiUrl(authOptions)).toEqual(
-      API.PROTOCOL
-      + "://"
-      + authOptions.projectID
-      + "."
-      + authOptions.zone
-      + "."
-      + API_SUFFIX,
-    );
+    expect(getApiUrl(authOptions)).toEqual([
+      API.PROTOCOL,
+      authOptions.projectID + ".",
+      getZone(authOptions.zone) + ".",
+      API_SUFFIX,
+    ].join(""));
   });
 
   it("should return given project URL when provided", () => {
     const projectURL = faker.internet.url();
     const authOptions = createAuthOptions({ projectURL });
 
-    expect(getApiUrl(authOptions)).toEqual(projectURL);
-  });
-
-  it("should return url with given zone", () => {
-    const authOptions = createAuthOptions();
-
-    expect(getApiUrl(authOptions)).toContain("." + authOptions.zone);
-  });
-
-  it(`should return url with default zone when given zone is undefined`, () => {
-    const authOptions = { projectID: faker.random.uuid() };
-
-    expect(getApiUrl(authOptions)).toContain("." + DEFAULT_PROJECT_ZONE);
+    expect(getApiUrl(authOptions)).toEqual(stripUrlSlashes(projectURL));
   });
 
   ["", null].forEach((zone) => {
@@ -50,6 +42,84 @@ describe("get api url", () => {
       const authOptions = createAuthOptions({ zone });
 
       expect(getApiUrl(authOptions)).toContain("." + DEFAULT_PROJECT_ZONE);
+    });
+  });
+});
+
+describe("get rtc url", () => {
+  const generateToken = () => {
+    const token = faker.random.alphaNumeric(36);
+    return { token, expectedParam: `?access_token=${token}` };
+  };
+
+  it("should return composed url", () => {
+    const authOptions = createAuthOptions();
+    const { token, expectedParam } = generateToken();
+
+    expect(getRtcUrl(authOptions, token)).toEqual([
+      API.REAL_TIME.PROTOCOL,
+      authOptions.projectID + ".",
+      getZone(authOptions.zone) + ".",
+      REALTIME_SUFFIX,
+      expectedParam,
+    ].join(""));
+  });
+
+  it("should return given project URL formatted", () => {
+    const domain = faker.internet.domainName();
+    const projectURL = `${faker.internet.protocol()}://${domain}`;
+    const authOptions = createAuthOptions({ projectURL });
+    const { token, expectedParam } = generateToken();
+
+    expect(getRtcUrl(authOptions, token)).toEqual([
+      API.REAL_TIME.PROTOCOL,
+      domain,
+      API.REAL_TIME.ENDPOINT,
+      expectedParam,
+    ].join(""));
+  });
+
+  ["", null].forEach((zone) => {
+    it(`should return url with default zone when given zone is "${zone}"`, () => {
+      const authOptions = createAuthOptions({ zone });
+      const { token } = generateToken();
+
+      expect(getRtcUrl(authOptions, token)).toContain("." + DEFAULT_PROJECT_ZONE);
+    });
+  });
+});
+
+describe("get zone", () => {
+  it("should return given zone when isn't nullish", () => {
+    const zone = faker.random.word();
+
+    expect(getZone(zone)).toEqual(zone);
+  });
+
+  [undefined, null, ""].forEach((zone) => {
+    it(`should return default zone when provided one is "${zone}"`, () => {
+      expect(getZone(zone)).toEqual(DEFAULT_PROJECT_ZONE);
+    });
+  });
+});
+
+describe("strip url slashes", () => {
+  it("should return given project URL without changes when it doesn't contain slashes", () => {
+    const projectURL = faker.internet.url();
+
+    expect(stripUrlSlashes(projectURL)).toEqual(projectURL);
+  });
+
+  it("should strip trailling slashes", () => {
+    const projectURL = faker.internet.url();
+    const times = faker.random.number({ min: 1, max: 10 });
+
+    expect(stripUrlSlashes(projectURL + "/".repeat(times))).toEqual(projectURL);
+  });
+
+  [undefined, null, ""].forEach((url) => {
+    it(`should return empty string when url is "${url}"`, () => {
+      expect(stripUrlSlashes(url)).toEqual("");
     });
   });
 });
