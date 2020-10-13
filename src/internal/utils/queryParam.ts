c54fb730dfe0a006ed4a12e94110354a2825138b
Marlos Borges <marlos.borges@gmail.com>
2020-10-13 08:38:06 +0200

    refactor(request-executer): extract method that parses query params (#178)

--- src/internal/utils/queryParam.ts
+++ src/internal/utils/queryParam.ts
@@ -1,5 +1,10 @@
 export type QueryParam = { key: string; value: any; };
 
+const encodeValue = (v: any) => encodeURIComponent(
+  typeof v === "string" ? v : JSON.stringify(v)
+);
+const toQueryParam = ({ key, value }: QueryParam) => `${key}=${encodeValue(value)}`;
+
 /**
  * Maps an object into a list of QueryParam
  * @param   o The object to be mapped
@@ -7,4 +12,20 @@ export type QueryParam = { key: string; value: any; };
  */
 export function toQueryParams(o: object): QueryParam[] {
   return Object.entries(o).map(([key, value]) => ({ key, value }));
+}
+
+/**
+ * Parses query params elements into query string format
+ *
+ * @param  queryParams
+ * @returns string
+ */
+export function parseQueryParams(queryParams: QueryParam[] = []): string {
+  if (queryParams && !queryParams.length) {
+    return "";
+  }
+
+  return "?" + queryParams
+    .map(toQueryParam)
+    .join("&");
 }
